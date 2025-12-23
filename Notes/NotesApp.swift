a11a8b165d39408ebfde3769f75206ce80e18041
Hesham <flex>
2025-12-23 03:16:17 +0200

    Initial Commit

---
+++ Notes/NotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesApp.swift
+//  Notes
+//
+//  Created by Mahmoud Emad on 23/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct NotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
